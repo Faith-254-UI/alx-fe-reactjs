86be55ea3e2e73e8a87319bd3ff2a818a1f547ed
Faith-254-UI <nivafaith@gmail.com>
2025-08-25 23:26:25 +0300

    Update project with recent corrections

--- alx-react-app/src/main.tsx
+++ alx-react-app/src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App from './App' // no .jsx needed, TS handles it
 
-createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById('root') as HTMLElement
+
+createRoot(rootElement).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
