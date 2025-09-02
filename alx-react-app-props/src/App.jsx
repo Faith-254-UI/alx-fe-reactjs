4c46248352a05a46b404fd5b7d43b7fde3600221
Faith-254-UI <nivafaith@gmail.com>
2025-09-02 23:05:05 +0300

    Refactor: Context API implementation in JSX

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,13 +1,16 @@
 // src/App.jsx
-import ProfilePage from './ProfilePage';
 import UserContext from './UserContext';
+import UserProfile from './UserProfile';
+
+const userData = {
+  name: "Jane Doe",
+  email: "jane.doe@example.com"
+};
 
 function App() {
-  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
-
   return (
     <UserContext.Provider value={userData}>
-      <ProfilePage />
+      <UserProfile />
     </UserContext.Provider>
   );
 }
