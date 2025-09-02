a1533f66d80a834304391ee3ddef1f0d92f3096b
Faith-254-UI <nivafaith@gmail.com>
2025-09-02 22:18:11 +0300

    Fix: Added inline styling to Navbar component

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,11 +2,18 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav style={{ padding: "10px", background: "#333", color: "#fff" }}>
-      <Link to="/" style={{ margin: "0 10px", color: "#fff", textDecoration: "none" }}>Home</Link>
-      <Link to="/about" style={{ margin: "0 10px", color: "#fff", textDecoration: "none" }}>About</Link>
-      <Link to="/services" style={{ margin: "0 10px", color: "#fff", textDecoration: "none" }}>Services</Link>
-      <Link to="/contact" style={{ margin: "0 10px", color: "#fff", textDecoration: "none" }}>Contact</Link>
+    <nav
+      style={{
+        backgroundColor: "#333",
+        display: "flex",
+        justifyContent: "space-around",
+        padding: "1rem",
+      }}
+    >
+      <Link to="/" style={{ color: "white", textDecoration: "none" }}>Home</Link>
+      <Link to="/about" style={{ color: "white", textDecoration: "none" }}>About</Link>
+      <Link to="/services" style={{ color: "white", textDecoration: "none" }}>Services</Link>
+      <Link to="/contact" style={{ color: "white", textDecoration: "none" }}>Contact</Link>
     </nav>
   );
 }
