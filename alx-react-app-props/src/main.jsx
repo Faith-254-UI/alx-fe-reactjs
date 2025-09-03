5756dacda088bdfbe5c3d41475689e2578863672
Faith-254-UI <nivafaith@gmail.com>
2025-09-03 17:35:16 +0300

    Update main.jsx with correct UserProvider wrapping

--- alx-react-app-props/src/main.jsx
+++ alx-react-app-props/src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { UserProvider } from "./UserContext";
 
-// Wrap App with UserProvider
 ReactDOM.createRoot(document.getElementById("root")).render(
   <UserProvider>
     <App />
