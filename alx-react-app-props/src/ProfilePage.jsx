41839c40ec2b8ed716b7f0161d60541cb64ba47d
Faith-254-UI <nivafaith@gmail.com>
2025-09-05 23:04:58 +0300

    feat: refactor prop drilling to use Context API

---
+++ alx-react-app-props/src/ProfilePage.jsx
@@ -0,0 +1,7 @@
+import UserInfo from './UserInfo';
+
+function ProfilePage() { // Remove userData from props
+  return <UserInfo />; // Remove userData prop here
+}
+
+export default ProfilePage;
---
+++ alx-react-app-props/src/UserInfo.jsx
@@ -0,0 +1,7 @@
+import UserDetails from './UserDetails';
+
+function UserInfo() { // Remove userData from props
+  return <UserDetails />; // Remove userData prop here
+}
+
+export default UserInfo;
