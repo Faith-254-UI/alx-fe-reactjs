f901b35971a7bbfc83d1d3dcfd18a4b9eb876701
Faith-254-UI <nivafaith@gmail.com>
2025-09-06 22:10:00 +0300

    update recent changes

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,13 +1,5 @@
-import React, { createContext } from 'react';
+import { createContext } from 'react';
 
 const UserContext = createContext();
-
-export const UserProvider = ({ children, value }) => {
-  return (
-    <UserContext.Provider value={value}>
-      {children}
-    </UserContext.Provider>
-  );
-};
 
 export default UserContext;
