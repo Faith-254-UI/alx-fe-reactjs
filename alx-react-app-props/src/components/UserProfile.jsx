f09e03e8d5490183bf691f5f5583fa4e9362d5fa
Faith-254-UI <nivafaith@gmail.com>
2025-09-07 17:17:53 +0300

    Fix UserProfile.jsx to use UserContext directly

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,9 +1,16 @@
-﻿import UserInfo from './UserInfo';
+﻿import { useContext } from 'react';
+import UserContext from './UserContext';
 
 function UserProfile() {
+  const userData = useContext(UserContext);
+
   return (
     <div>
-      <UserInfo />
+      <h1>User Profile</h1>
+      <h2>{userData.name}</h2>
+      <p>Age: {userData.age}</p>
+      <p>Bio: {userData.bio}</p>
+      <p>Email: {userData.email}</p>
     </div>
   );
 }
